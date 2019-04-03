c96efd75fa702ba9956d244969db48b773665c42
awanla <48355798+awanla@users.noreply.github.com>
2019-04-03 13:59:52 +0800

    完整

--- js/all.js
+++ js/all.js
@@ -12,26 +12,29 @@ $(document).ready(function () {
         }
         var scrollDistance = $(window).scrollTop();
             if (scrollDistance > 350) {
-                $(".news_text").css("animation-play-state", "running");
-                $(".news_box").css("animation-play-state", "running");
+                $(".news_title").addClass("news_title_animation");
+                $(".news_box").addClass("news_box_animation");
             }
         var scrollDistance = $(window).scrollTop();
-            if (scrollDistance > 1250) {
-                $(".about_skew_animation").css("animation-play-state", "running");
-                $(".about_title").css("animation-play-state", "running");
+            if (scrollDistance > 1000) {
+                $(".skew_right").addClass("skew_right_animation");
+                $(".about_title").addClass("about_title_animation");
+                $(".skew_right_vido").addClass("skew_right_vido_ani");
+                $(".about_title").addClass("about_title_animation");
+                $(".about_text").addClass("about_text_animation");
             }
         var scrollDistance = $(window).scrollTop();
-            if (scrollDistance > 2100) {
-                $(".skew_animate").css("animation-play-state", "running");
-                $(".service_pic .pic").css("animation-play-state", "running");
-                $(".service_tilte").css("animation-play-state", "running");
-                
+            if (scrollDistance > 2000) {
+                $(".skew_left").addClass("skew_left_animation");
+                $(".skew_left_pic").addClass("skew_left_pic_animation");
+                $(".service_text").addClass("service_text_animation");
+
             }
         var scrollDistance = $(window).scrollTop();
             if (scrollDistance > 3000) {
-                $(".contect_animate").css("animation-play-state", "running");
-                $(".container_title h1").css("animation-play-state", "running");
-                $(".container_text").css("animation-play-state", "running");
+                $(".skew_right_contect").addClass("skew_right_contect_ani");
+                $(".skew_right_map").addClass("skew_right_map_ani");
+                $(".contect_text").addClass("contect_text_animation");
 
             }
     });
@@ -41,6 +44,12 @@ $(document).ready(function () {
             scrollTop: $('html').offset().top
         });
 
+    });
+    $('.content_icon').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 1000
+        }, 1000);
     });
 
 
